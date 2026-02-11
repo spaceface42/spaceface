695a4b2e515a121f1a5e665b5282a872c884ad2f
Zsolt Sándor <ezsolt@gmail.com>
2026-02-11 11:23:46 +0100

    built js

--- docs/bin/bundle.js
+++ docs/bin/bundle.js
@@ -2911,43 +2911,19 @@ var SpacefaceCore = class _SpacefaceCore {
   }
 };
 
-// src/app/main.ts
+// src/app/main.prod.ts
 var app = new SpacefaceCore({
   features: {
-    partialLoader: { enabled: true, debug: true, baseUrl: "/", cacheEnabled: true },
     slideplayer: { interval: 5e3, includePicture: false },
     screensaver: { delay: 4500, partialUrl: "content/feature/screensaver/index.html" },
     serviceWorker: true
   }
 });
 app.initBase().then(async () => {
-  await app.initPartialLoader();
   await app.initDomFeatures();
   await app.initOnceFeatures();
   app.finishInit();
 });
-var isDev = ["localhost", "127.0.0.1"].some(
-  (host) => window.location.hostname.includes(host)
-);
-if (isDev) {
-  eventBus.onAny((eventName, payload) => {
-    if (eventName === "log:debug") return;
-    if (eventName === "log" && payload?.level === "debug") return;
-    const { level = "log", args, ...otherDetails } = payload ?? {};
-    if (!payload) return console.log(`[spaceface onAny] Event: ${eventName} \u2013 no payload!`);
-    if (typeof payload === "string") return console.log(`[spaceface onAny] Event: ${eventName} [LOG]`, payload);
-    const fullMessage = args ?? otherDetails ?? "(no details)";
-    const methodMap = {
-      debug: "debug",
-      info: "info",
-      warn: "warn",
-      error: "error",
-      log: "log"
-    };
-    const method = methodMap[level] ?? "log";
-    console[method](`[SPCFC *] Event: ${eventName} [${String(level).toUpperCase()}] \u2013`, fullMessage);
-  });
-}
 window.addEventListener("beforeunload", () => {
   app.destroy();
   app.log("info", "App destroyed on beforeunload");
