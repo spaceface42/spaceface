9136508db34b38b03a74aa30d588afb2519c6dae
graphis <ezsolt@gmail.com>
2026-03-10 21:09:41 +0100

    added back originap php server util to be able to start on win and on mac also

--- bin/start-php-server.php
+++ bin/start-php-server.php
@@ -1,26 +1,59 @@
 <?php
+
 declare(strict_types=1);
 
-$phpExe = 'C:/applications/php/php.exe';
-$docRoot = dirname(__DIR__) . '/docs';
-$host = $argv[1] ?? '127.0.0.1';
-$port = $argv[2] ?? '8000';
+$defaults = [
+    'host' => '127.0.0.1',
+    'port' => '8000',
+    'root' => 'docs',
+];
 
-if (!is_file($phpExe)) {
-    fwrite(STDERR, "PHP executable not found: {$phpExe}\n");
+$options = $defaults;
+
+foreach (array_slice($argv, 1) as $arg) {
+    if (str_starts_with($arg, '--port=')) {
+        $options['port'] = substr($arg, 7);
+        continue;
+    }
+
+    if (str_starts_with($arg, '--root=')) {
+        $options['root'] = substr($arg, 7);
+        continue;
+    }
+
+    if (str_starts_with($arg, '--host=')) {
+        $options['host'] = substr($arg, 7);
+        continue;
+    }
+
+    fwrite(STDERR, "Unknown option: {$arg}\n");
+    fwrite(STDERR, "Usage: php bin/start-server.php [--host=HOST] [--port=PORT] [--root=ROOT]\n");
     exit(1);
 }
 
-if (!is_dir($docRoot)) {
-    fwrite(STDERR, "Document root not found: {$docRoot}\n");
+$rootPath = $options['root'];
+if (!is_dir($rootPath)) {
+    fwrite(STDERR, "Root directory not found: {$rootPath}\n");
     exit(1);
 }
 
-$address = "{$host}:{$port}";
-$cmd = sprintf('"%s" -S %s -t "%s"', $phpExe, $address, $docRoot);
+$port = $options['port'];
+if (!preg_match('/^\d+$/', $port) || (int) $port < 1 || (int) $port > 65535) {
+    fwrite(STDERR, "Invalid port: {$port}\n");
+    exit(1);
+}
 
-echo "Starting PHP built-in server at http://{$address}\n";
-echo "Document root: {$docRoot}\n\n";
+$host = $options['host'];
+$url = "http://{$host}:{$port}";
+
+fwrite(STDOUT, "Starting PHP server at {$url} (root: {$rootPath})\n");
+
+$cmd = sprintf(
+    '%s -S %s -t %s',
+    escapeshellarg(PHP_BINARY),
+    escapeshellarg("{$host}:{$port}"),
+    escapeshellarg($rootPath)
+);
 
 passthru($cmd, $exitCode);
 exit($exitCode);
